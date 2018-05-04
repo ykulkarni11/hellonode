63e3181848cbc285f8f24b8546f9e0d5872706e8
ykulkarni11 <35995848+ykulkarni11@users.noreply.github.com>
2018-05-04 18:35:17 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
-docker pull yogiraj11/storeweb:master
-docker run -d -p 8000:8000 yogiraj11/storeweb:master
+docker pull yogiraj11/docker-hub-credentials
+docker run -d -p 8000:8000 yogiraj11/docker-hub-credentials
